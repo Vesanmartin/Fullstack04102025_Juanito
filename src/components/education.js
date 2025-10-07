1691142737b4c713257b44999d977720e3b58458
PAAWSA02LC1310 <Duoc@PAAWSA02LC1310.duoc.acad>
2025-10-07 20:33:51 -0300

    0710202 pruebat

---
+++ src/components/education.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+function Education
+{
+    const MiEducacion=[
+        titulo="Analista",
+        institucion="DuocUc",
+        ubicacion ="Los Vilos",
+        periodo ="2021"
+    ]
+
+}
+
+    return (
+        <section className='mb-5'>
+            <div className='card-shadow-sm'>
+                <div className='card-header bg-white'>
+                    <h2 className='h4- mb-0 text-primary'>
+                        Educacion:
+                    </h2>
+                </div>
+            </div>
+        </section>
+    );
+export default Education;
